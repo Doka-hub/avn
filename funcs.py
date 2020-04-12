1b134343531659dc0be14b5ac9685e0a493d9df9
Doka-hub <57629059+Doka-hub@users.noreply.github.com>
2020-04-12 07:39:57 +0600

    first

---
+++ funcs.py
@@ -0,0 +1,13 @@
+import requests
+
+
+def enter(login, password):
+    session = requests.Session()
+    session.post(
+        'https://avn.kstu.kg/lms/Index.aspx',
+        data={
+            'ctl00$LoginControl1$LoginTextbox': login,
+            'ctl00$LoginControl1$PasswordTextbox': password,
+            'ctl00$LoginControl1$ConnectButton': 'Войти'
+        }
+    )
---
+++ main.py
@@ -0,0 +1,47 @@
+import celery
+from celery.schedules import crontab
+from datetime import datetime
+import funcs
+import lessons
+
+app = celery.Celery('main', broker='redis://localhost:6379/0', backend='redis://localhost:6379/0')
+app.conf.timezone = 'Asia/Bishkek'
+app.conf.beat_schedule = {
+    '08.00': {
+        'task': 'main.task',
+        'schedule': crontab(hour=8),
+        'args': (r'18\30975', '123456')
+    },
+    '9.30': {
+        'task': 'main.task',
+        'schedule': crontab(hour=9, minute=30),
+        'args': (r'18\30975', '123456')
+    },
+    '11.00': {
+        'task': 'main.task',
+        'schedule': crontab(hour=11),
+        'args': (r'18\30975', '123456')
+    },
+    '13.00': {
+        'task': 'main.task',
+        'schedule': crontab(hour=13),
+        'args': (r'18\30975', '123456')
+    },
+    '14.30': {
+        'task': 'main.task',
+        'schedule': crontab(hour=14, minute=30),
+        'args': (r'18\30975', '123456')
+    },
+    '16.00': {
+        'task': 'main.task',
+        'schedule': crontab(hour=16),
+        'args': (r'18\30975', '123456')
+    }
+}
+
+
+@app.task
+def task(login, password):
+    day = int(datetime.today().strftime('%w'))
+    if day in range(1, 6):
+        funcs.enter(login, password)
